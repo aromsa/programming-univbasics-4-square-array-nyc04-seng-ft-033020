b1ed7d9c0a3cacb19fdf300984464e58ed908b49
anna bateman <annacbateman@gmail.com>
2020-03-23 23:17:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,9 +9,9 @@ end
   
   
   
-  counter = 0 
-  while counter < new_array.length do
-    puts new_array [counter]
-    counter += 1
-  end
-end
+  #counter = 0 
+  #while counter < new_array.length do
+   # puts new_array [counter]
+    #counter += 1
+#  end
+#end
